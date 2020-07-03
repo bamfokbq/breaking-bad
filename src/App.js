da885124ff5e49c8964e2d23e6e47963eb4fc12d
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2020-07-03 02:16:42 +0100

    Final project of Brad Breaking-bad-cast

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './components/UI/Header';
+import CharactersGrid from './components/Characters/CharactersGrid';
 import './App.css';
+import Search from './components/UI/Search';
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const results = await axios(
+        `https://breakingbadapi.com/api/characters?name=${query}`
+      );
+
+      setItems(results.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header />
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharactersGrid items={items} isLoading={isLoading} />
     </div>
   );
-}
+};
 
 export default App;
